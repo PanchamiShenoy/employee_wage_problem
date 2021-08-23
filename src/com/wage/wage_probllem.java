b5e59dec419e9ae71b8657e4f19b5492860d451e
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-23 14:48:44 +0530

    functionality to check attendance is added

--- src/com/wage/wage_probllem.java
+++ src/com/wage/wage_probllem.java
@@ -5,6 +5,16 @@ public class wage_probllem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("welcome to employee program");
+    int IS_FULL_TIME =1;
+    double empCheck=Math.floor(Math.random()*10)%2;
+    if (empCheck==IS_FULL_TIME)
+    	System.out.println("Employee is present");
+    else
+    	System.out.println("Employee is absent"); 
+    	
+    	}
+
+    
+
 	}
 
-}
