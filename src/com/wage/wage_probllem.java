044fd8dd027f0a4735a515fff8e6d0005569b143
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-23 15:18:52 +0530

    functionality to compute daily wage is added

--- src/com/wage/wage_probllem.java
+++ src/com/wage/wage_probllem.java
@@ -6,11 +6,16 @@ public class wage_probllem {
 		// TODO Auto-generated method stub
     System.out.println("welcome to employee program");
     int IS_FULL_TIME =1;
+    int ratePerHour =20;
+    int empHour=0;
+    int empWage=0;
     double empCheck=Math.floor(Math.random()*10)%2;
     if (empCheck==IS_FULL_TIME)
-    	System.out.println("Employee is present");
-    else
-    	System.out.println("Employee is absent"); 
+    	empHour=8;
+    	empWage=empHour*ratePerHour ;
+    			
+    	System.out.println("Employee wage is "+empWage);
+    
     	
     	}
 
