a6c4656234fc868c6957e0d5d2c6728c158b99ea
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-23 14:29:36 +0530

    initial welcome message added

---
+++ src/com/wage/wage_probllem.java
@@ -0,0 +1,10 @@
+package com.wage;
+
+public class wage_probllem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("welcome to employee program");
+	}
+
+}
