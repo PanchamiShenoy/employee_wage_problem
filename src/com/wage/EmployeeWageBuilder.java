065b5db2ed7b65b4bf70f499d864b8481997485c
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-09-05 11:16:30 +0530

    manage wage of multiple companies

--- src/com/wage/EmployeeWageBuilder.java
+++ src/com/wage/EmployeeWageBuilder.java
@@ -4,25 +4,32 @@ public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 
-	private final String COMPANY_NAME;
-	private final int RATE_PER_HOUR;
-	private final int MAX_DAYS;
-	private final int MAX_HOURS;
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArr;
 
-	EmployeeWageBuilder(String COMPANY_NAME, int RATE_PER_HOUR, int MAX_HOURS, int MAX_DAYS) {
-		this.COMPANY_NAME = COMPANY_NAME;
-		this.RATE_PER_HOUR = RATE_PER_HOUR;
-		this.MAX_DAYS = MAX_DAYS;
-		this.MAX_HOURS = MAX_HOURS;
+	EmployeeWageBuilder() {
+		companyEmpWageArr = new CompanyEmpWage[5];
 	}
 
-	public void computeWage() {
+	public void addCompanyEmpWage(String company, int ratePerHour, int maxHours, int maxDays) {
+		companyEmpWageArr[numOfCompany] = new CompanyEmpWage(company, ratePerHour, maxHours, maxDays);
+		numOfCompany++;
+	}
+
+	private void computeEmpWage() {
+		for (int i = 0; i < numOfCompany; i++) {
+			companyEmpWageArr[i].setTotalEmpWage(this.computeWage(companyEmpWageArr[i]));
+			System.out.println(companyEmpWageArr[i]);
+		}
+	}
+
+	public int computeWage(CompanyEmpWage companyEmpWage) {
 		int empHour = 0;
 		int empWage = 0;
 		int totalWage = 0;
 		int totalEmpHours = 0;
 		int totalEmpDays = 0;
-		while (totalEmpHours < MAX_HOURS && totalEmpDays < MAX_DAYS) {
+		while (totalEmpHours < companyEmpWage.maxHours && totalEmpDays < companyEmpWage.maxDays) {
 			totalEmpDays += 1;
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 			switch (empCheck) {
@@ -36,20 +43,22 @@ public class EmployeeWageBuilder {
 				empHour = 0;
 			}
 			totalEmpHours += empHour;
-			empWage = empHour * RATE_PER_HOUR;
+			empWage = empHour * companyEmpWage.ratePerHour;
 			totalWage += empWage;
 		}
-		System.out.println("\ncompany name is" + COMPANY_NAME);
+		System.out.println("\ncompany name is" + companyEmpWage.company);
 		System.out.println("Employee wage is " + totalWage);
 		System.out.println("Total working hours is " + totalEmpHours);
 		System.out.println("Total working days is " + totalEmpDays);
+		return totalWage;
 	}
 
 	public static void main(String args[]) {
 
-		EmployeeWageBuilder yml = new EmployeeWageBuilder("YML", 30, 160, 20);
-		yml.computeWage();
-		EmployeeWageBuilder bl = new EmployeeWageBuilder("BridgeLabz", 30, 150, 15);
-		bl.computeWage();
+		EmployeeWageBuilder empWageBuilder = new EmployeeWageBuilder();
+
+		empWageBuilder.addCompanyEmpWage("YML", 30, 140, 20);
+		empWageBuilder.addCompanyEmpWage("bridgelabz", 20, 100, 20);
+		empWageBuilder.computeEmpWage();
 	}
 }
